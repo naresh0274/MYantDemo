060d0228a6210f71501ee472f064ca8652d5d289
naresh0274 <pnarendharreddy92@gmail.com>
2018-12-08 13:55:56 -0500

    hello this is my project

---
+++ src/math/Main.java
@@ -0,0 +1,14 @@
+package math;
+
+public class Main {
+
+
+		
+		public static void main(String[] args) {
+			 MyMath math = new MyMath();
+		        System.out.println("Result is: " + math.multi(5, 10));
+		}
+
+	}
+
+
